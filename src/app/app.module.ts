0371f0b61c913a0d09db8c4b6be4bd535cb01135
DMoreno_06 <mordiana1506@gmail.com>
2019-04-21 18:15:32 -0500

    schema barbusto's

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LogoComponent } from './logo/logo.component';
+import { BannerComponent } from './banner/banner.component';
+import { MenuComponent } from './menu/menu.component';
+import { SliderComponent } from './slider/slider.component';
+import { FooterComponent } from './footer/footer.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutComponent } from './about/about.component';
+import { FeaturesComponent } from './features/features.component';
+import { EmployeesComponent } from './employees/employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent,
+    BannerComponent,
+    MenuComponent,
+    SliderComponent,
+    FooterComponent,
+    PortfolioComponent,
+    GalleryComponent,
+    ContactComponent,
+    ServicesComponent,
+    AboutComponent,
+    FeaturesComponent,
+    EmployeesComponent
   ],
   imports: [
     BrowserModule
