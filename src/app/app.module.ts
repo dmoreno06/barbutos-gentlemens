efe1356eb746c005f01dab5c6a05e9e3ec6f8692
DMoreno_06 <mordiana1506@gmail.com>
2019-04-22 22:08:32 -0500

    new schema barbusto's

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LogoComponent } from './logo/logo.component';
-import { BannerComponent } from './banner/banner.component';
-import { MenuComponent } from './menu/menu.component';
-import { SliderComponent } from './slider/slider.component';
-import { FooterComponent } from './footer/footer.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { ContactComponent } from './contact/contact.component';
+import { LogoComponent } from './shared/logo/logo.component';
+import { BannerComponent } from './shared/banner/banner.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { SliderComponent } from './pages/slider/slider.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ContactComponent } from './pages/contact/contact.component';
 import { ServicesComponent } from './services/services.component';
-import { AboutComponent } from './about/about.component';
-import { FeaturesComponent } from './features/features.component';
-import { EmployeesComponent } from './employees/employees.component';
+import { AboutComponent } from './pages/about/about.component';
+import { FeaturesComponent } from './pages/features/features.component';
+import { EmployeesComponent } from './pages/employees/employees.component';
 
 @NgModule({
   declarations: [
